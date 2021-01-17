489158ac62a8cf8f14a39c85d1657132fded0a1c
kushalshm1 <sharmakushal2k@gmail.com>
2021-01-17 21:26:10 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 from datetime import date
 from models import company
-# app = Flask(__name__)
 
-# @app.route('/')
-# def basic():
-#     d = date.today()
-#     flag = 1
-#     text1 = 'If Text'
-#     text2 = 'Else Text'
-#     return render_template('index.html',date = d,f = flag,t1 = text1,t2 = text2)
+app = Flask(__name__)
 
-# if __name__=='__main__':
-#     app.run(debug=True)
-#     app.run()
+@app.route('/')
+def basic():
+    d = date.today()
+    flag = 1
+    text1 = 'If Text'
+    text2 = 'Else Text'
+    return render_template('index.html',date = d,f = flag,t1 = text1,t2 = text2)
+
+if __name__=='__main__':
+    app.run(debug=True)
+    app.run()
